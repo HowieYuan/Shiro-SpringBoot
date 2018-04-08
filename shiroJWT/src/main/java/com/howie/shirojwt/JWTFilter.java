67d4f6f20aba15a8562e0e0c2a0389494e2ad77b
swit <swithaoy@gmail.com>
2018-04-08 22:22:54 +0800

    jwt

---
+++ shiroJWT/src/main/java/com/howie/shirojwt/JWTFilter.java
@@ -0,0 +1,14 @@
+package com.howie.shirojwt;
+
+import org.apache.shiro.web.filter.authc.BasicHttpAuthenticationFilter;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @Author yuanhaoyue swithaoy@gmail.com
+ * @Description
+ * @Date 2018-04-08
+ * @Time 12:36
+ */
+public class JWTFilter extends BasicHttpAuthenticationFilter {
+}
